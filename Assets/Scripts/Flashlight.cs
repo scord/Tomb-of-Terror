5916d08761b29d2469f342b7b58d43de99a458d8
Paul <pp13003@my.bristol.ac.uk>
2015-10-22 18:24:46 +0100

    box with flash

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	
+	void Start(){
+		offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate(){
+		transform.position = player.transform.position + offset;
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+	}
+}
---
+++ Assets/Scripts/playerController1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController1 : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	public float rotateSpeed;
+	private float curSpeed;
+	public GameObject flashlight;
+	public float flashSpeed;
+	
+	void Start(){
+		rb =GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+	 	
+		
+		Vector3 movement = new Vector3 (0.0f, 0.0f, moveVertical);
+		Vector3 rotate = new Vector3 ( 0.0f, moveHorizontal, 0.0f);
+		transform.Rotate (rotate * rotateSpeed);
+
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		curSpeed = speed * moveVertical;
+		if (moveVertical == 0.0f) {
+			flashlight.GetComponent<Light>().intensity -= flashSpeed;
+		} else {
+			flashlight.GetComponent<Light>().intensity += flashSpeed;
+		}
+
+		rb.MovePosition (transform.position+ forward*curSpeed);
+	}
+
+	
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag("pickup"))
+			other.gameObject.SetActive(false);
+		
+	}	
+}
