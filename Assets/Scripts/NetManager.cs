4cba1327a40587d2849303e2b39a830b6235f88b
asomanescu <as13118@my.bristol.ac.uk>
2016-01-27 11:48:23 +0000

    local ip?

--- Assets/Scripts/NetManager.cs
+++ Assets/Scripts/NetManager.cs
@@ -29,7 +29,7 @@ public class NetManager : NetworkManager
     {
         chosenPlayerIndex = playerId; 
         NetworkManager.singleton.networkPort = 7777;
-        NetworkManager.singleton.networkAddress = "localhost";
+        NetworkManager.singleton.networkAddress = "172.23.255.254";
         if (host)
             NetworkManager.singleton.StartHost();
         else
