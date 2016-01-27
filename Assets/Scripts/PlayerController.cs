a124d22b4a6f7ae7e6d32ea9ec02adee8fd36a8a
SimonBai <xb13271@my.bristol.ac.uk>
2016-01-27 14:42:27 +0000

    footstep sound added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,17 @@ public class PlayerController : MonoBehaviour {
 
     public Camera cam;
 	public SoundVision soundVision;
+    public AudioSource audio_source;
 
     bool turned;
 	// public SoundVision test;
     // Use this for initialization
     void Start () {
 	    turned = false;
-	}
+
+        audio_source = GetComponent<AudioSource>();
+        audio_source.clip = (AudioClip)Resources.Load("AudioClips/Footstep1");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,6 +26,14 @@ public class PlayerController : MonoBehaviour {
         if (moveVertical == 1.0)
         {
             transform.position = transform.position + cam.transform.forward * 2 * Time.deltaTime;
+            if (audio_source.isPlaying == false)// add more logic later such as, onground/jumping etc etc
+            {
+                //AudioSource.PlayClipAtPoint(footstep_Sound1, transform.position);
+                //footstep_playing = 1;
+                audio_source.pitch = Random.Range(0.8f, 1);
+                audio_source.volume = Random.Range(0.8f, 1.1f);
+                audio_source.Play();
+            }
         }
 
         if ((moveHorizontal == 1.0 || moveHorizontal == -1.0) && turned == false)
