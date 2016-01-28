83444457a9656e618ca5d81e05c237690b886e1b
Greg Cawthorne <cawthornegd@gmail.com>
2016-01-28 12:08:05 +0000

    Heart Rate Python Addition

---
+++ Assets/Scripts/HeartRateManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System;
+
+public class HeartRateManager : MonoBehaviour {
+	Thread thread;
+	bool programActive = true;
+	// Use this for initialization
+	void Start () {
+
+		object path = Application.dataPath;
+		thread = new Thread(new ParameterizedThreadStart(ProcessData));
+		thread.Start(path);
+	}
+
+	void ProcessData(object ob){
+		string path = ob.ToString();
+
+		Debug.Log ("Thread started");
+
+		while (programActive) {
+
+			Debug.Log(path+"/Resources/HR.txt");
+			string text = System.IO.File.ReadAllText(path+"/Resources/HR.txt");
+			int hr = Convert.ToInt32(text);
+
+			System.Threading.Thread.Sleep(350);
+		}
+		Debug.Log ("Thread stopped");
+	}
+
+	public void OnDisable(){
+		Debug.Log ("Heart Rate Disabled");
+		programActive = false;   
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
