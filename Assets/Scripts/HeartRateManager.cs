5a15953cd6b3a686244fe8ca42007665e3a74fcc
Greg Cawthorne <cawthornegd@gmail.com>
2016-04-01 17:40:20 +0100

    pls work

--- Assets/Scripts/HeartRateManager.cs
+++ Assets/Scripts/HeartRateManager.cs
@@ -9,6 +9,7 @@ public class HeartRateManager : NetworkBehaviour {
 	bool programActive = true;
 
 	System.Diagnostics.Process HRProcess;
+    System.Diagnostics.Process bluKillProcess;
 
 	//[SyncVar]
 	public int HeartRate;
@@ -19,13 +20,22 @@ public class HeartRateManager : NetworkBehaviour {
 		object path = Application.dataPath;
 		
 		HRProcess = new System.Diagnostics.Process();
-		HRProcess.StartInfo.FileName = path+"/Scripts/dist/bglib_test_hr_collector.exe";
+		HRProcess.StartInfo.FileName = path+"/Scripts/HR/bglib_test_hr_collector.exe";
  
 		HRProcess.StartInfo.UseShellExecute = false;
 		HRProcess.StartInfo.RedirectStandardOutput = true;
 		HRProcess.StartInfo.CreateNoWindow = true;
 		HRProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
 		
+        
+        bluKillProcess = new System.Diagnostics.Process();
+		bluKillProcess.StartInfo.FileName = "C:\\Windows\\system32\\cmd.exe";
+        bluKillProcess.StartInfo.Arguments = "/c" + path+"/Scripts/HR/bglibKill.bat";
+		bluKillProcess.StartInfo.UseShellExecute = false;
+		bluKillProcess.StartInfo.RedirectStandardOutput = true;
+		bluKillProcess.StartInfo.CreateNoWindow = true;
+		bluKillProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+        
 		HRProcess.Start();
 		
 		thread = new Thread(new ParameterizedThreadStart(ProcessData));
@@ -46,9 +56,20 @@ public class HeartRateManager : NetworkBehaviour {
 
 	public void OnDisable(){
             // Free resources associated with process.
-        HRProcess.Close();
+        programActive = false;
+        
+        bool stopped = false;
+        bluKillProcess.Start();
+        while(!stopped){
+            String output = bluKillProcess.StandardOutput.ReadLine();
+            stopped = output.Equals("done!");
+            
+        }
+        bluKillProcess.Kill();
+        bluKillProcess.WaitForExit();
+        
 		Debug.Log ("Heart Rate Disabled");
-		programActive = false;   
+  
 
 	}
 
