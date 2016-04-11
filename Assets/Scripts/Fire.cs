549e429a3d00ce82fe16742fb94715cef1deb023
Sam Cordingley <samcordingley@gmail.com>
2016-04-11 15:01:04 +0100

    fixing

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -4,13 +4,14 @@ using UnityEngine.Networking;
 
 public class Fire : TargetInteract{
 	[SyncVar (hook = "SyncActiveState")] private bool active = true;
-	private ParticleSystem m_ParticleSystem;
-  private AudioSource m_AudioSource;
+    [SerializeField]
+    private ParticleSystem[] m_ParticleSystems;
+    private AudioSource m_AudioSource;
   private Light m_Light;
 
 	private void Start() {
-		m_ParticleSystem = gameObject.GetComponent<ParticleSystem>();
-    m_AudioSource = gameObject.GetComponentInChildren<AudioSource>();
+        m_ParticleSystems = GetComponentsInChildren<ParticleSystem>();
+        m_AudioSource = gameObject.GetComponentInChildren<AudioSource>();
     m_Light = gameObject.GetComponentsInChildren<Light>(true)[0];
 		UpdateParticleSystem();
 	}
@@ -41,13 +42,38 @@ public class Fire : TargetInteract{
   	UpdateParticleSystem();
   }
 
-  private void UpdateParticleSystem() {
+
+    void Update()
+    {
+        SetLight();
+    }
+
+    void SetLight()
+    {
+        float intensity = m_Light.GetComponent<FireLight>().intensityMultiplier;
+        if (active)
+        {
+            m_Light.GetComponent<FireLight>().intensityMultiplier = Mathf.Lerp(intensity, 1.0f, 0.01f);
+        }
+        else
+        {
+            m_Light.GetComponent<FireLight>().intensityMultiplier = Mathf.Lerp(intensity, 0.0f, 0.05f);
+        }
+    }
+
+    private void UpdateParticleSystem() {
   	if (active) {
-  		m_ParticleSystem.Play();
+      foreach (ParticleSystem m_ParticleSystem in m_ParticleSystems)
+      {
+        m_ParticleSystem.Play();
+      }
       m_AudioSource.Play();
       m_Light.enabled = true;
   	} else {
-  		m_ParticleSystem.Stop();
+      foreach (ParticleSystem m_ParticleSystem in m_ParticleSystems)
+      {
+        m_ParticleSystem.Stop();
+      }
       m_AudioSource.Stop();
       m_Light.enabled = false;
   	}
