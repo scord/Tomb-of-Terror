4eb309f616bc010f6e92bd55353a5199d8424bf9
Paul <pp13003@my.bristol.ac.uk>
2016-04-20 23:34:28 +0100

    remove network identity on doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,49 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.Networking;
+//using UnityEngine.Networking;
 
-public class Door : TargetInteract{
-  [SyncVar (hook = "SyncDirectionState")] private int direction = 1;
+public class Door : MonoBehaviour{
+  //[SyncVar (hook = "SyncDirectionState")] private int direction = 1;
 
 	public AudioClip doorOn_sound;
 	public AudioClip doorOff_sound;
 
-	private bool gravity = true;
+	// private bool gravity = true;
 
 
-	public void Start(){
-    //GetComponent<Rigidbody>().velocity = transform.up * -3 * direction;
-		GetComponent<Rigidbody>().useGravity = gravity;
-	}
+	// public void Start(){
+  //   //GetComponent<Rigidbody>().velocity = transform.up * -3 * direction;
+	// 	GetComponent<Rigidbody>().useGravity = gravity;
+	// }
+  //
+	// public override string GetText(){
+	// 	return "open Door";
+	// }
 
-	public override string GetText(){
-		return "open Door";
-	}
+	// public override void Trigger(){
+  //
+  //   if (hasAuthority) {
+  //     gravity = !gravity;
+  //     GetComponent<Rigidbody>().useGravity = false;
+  //     GetComponent<Rigidbody>().velocity = transform.up * -3 * direction;
+  //     direction = -direction;
+  //   } else {
+  //     CmdSetDirection(-direction);
+  //   }
+	// }
 
-	public override void Trigger(){
-
-    if (hasAuthority) {
-      gravity = !gravity;
-      GetComponent<Rigidbody>().useGravity = false;
-      GetComponent<Rigidbody>().velocity = transform.up * -3 * direction;
-      direction = -direction;
-    } else {
-      CmdSetDirection(-direction);
-    }
-	}
-
-  [Command]
-  private void CmdSetDirection(int newDirection) {
-    direction = newDirection;
-  }
-
-  [Client]
-  private void SyncDirectionState(int newDirection) {
-    gravity = !gravity;
-    GetComponent<Rigidbody>().useGravity = false;
-
-    GetComponent<Rigidbody>().velocity = transform.up * -3 * direction;
-    direction = newDirection;
-  }
+  // [Command]
+  // private void CmdSetDirection(int newDirection) {
+  //   direction = newDirection;
+  // }
+  //
+  // [Client]
+  // private void SyncDirectionState(int newDirection) {
+  //   gravity = !gravity;
+  //   GetComponent<Rigidbody>().useGravity = false;
+  //
+  //   GetComponent<Rigidbody>().velocity = transform.up * -3 * direction;
+  //   direction = newDirection;
+  // }
 
 }
