f9844c446fef9fc62ecd2fffe732c035846c18bf
Greg <gregory@eduroam-149-088.nomadic.bris.ac.uk>
2016-04-23 11:28:50 +0100

    added mummy music controller

---
+++ Assets/Scripts/MummyMusicController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MummyMusicController : MonoBehaviour {
+
+		private Renderer explorerRenderer;
+
+		// Use this for initialization
+		void Start () {
+			GameObject explorer = GameObject.FindGameObjectWithTag("Explorer");
+
+			if (explorer != null){
+				explorerRenderer = explorer.GetComponentInChildren<Renderer>();
+			}
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+			GameObject explorer = GameObject.FindGameObjectWithTag("Explorer");
+
+			if (explorer != null){
+				explorerRenderer = explorer.GetComponentInChildren<Renderer>();
+			}
+
+			if (explorerRenderer != null && explorerRenderer.IsVisibleFrom(Camera.main)){
+				Debug.Log("explorer seen");
+			}
+		}
+	}
