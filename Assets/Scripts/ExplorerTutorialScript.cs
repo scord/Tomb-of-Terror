11a8c7985122e56ed5146df877246f43afdc4fbb
Anda Truta <contact@andatruta.com>
2016-04-25 22:01:10 +0100

    done all prompts (hopefully?) :raised_hands:

--- Assets/Scripts/ExplorerTutorialScript.cs
+++ Assets/Scripts/ExplorerTutorialScript.cs
@@ -4,13 +4,15 @@ using UnityEngine.UI;
 
 public class ExplorerTutorialScript : IntroTutorialScript {
 
-  [SerializeField] private GameObject headCanvas, walkCanvas, pivotCanvas, pickupCanvas, throwCanvas, gobackCanvas, runCanvas, extinguishCanvas;
+  [SerializeField] private GameObject headCanvas, walkCanvas, pivotCanvas, pickupCanvas, throwCanvas, gobackCanvas, runCanvas, extinguishCanvas, toTombCanvas, enterCanvas;
   [SerializeField] Text fireInfo;
   [SerializeField] private PlayerController playerController;
+  [SerializeField] private ExplorerController explorerController;
   [SerializeField] private Camera cam;
   [SerializeField] private GameObject explorerObject;
 
-  private int pivotCount = 5;
+  private int pivotCount = 4;
+  private bool showedExtinguish = false;
   // Use this for initialization
   protected void Start () {
     headCanvas.SetActive(true);
@@ -21,6 +23,8 @@ public class ExplorerTutorialScript : IntroTutorialScript {
     gobackCanvas.SetActive(false);
     runCanvas.SetActive(false);
     extinguishCanvas.SetActive(false);
+    toTombCanvas.SetActive(false);
+    enterCanvas.SetActive(false);
 
     walkCanvas.GetComponent<CanvasGroup>().alpha = 0;
     pivotCanvas.GetComponent<CanvasGroup>().alpha = 0;
@@ -29,6 +33,11 @@ public class ExplorerTutorialScript : IntroTutorialScript {
     gobackCanvas.GetComponent<CanvasGroup>().alpha = 0;
     runCanvas.GetComponent<CanvasGroup>().alpha = 0;
     extinguishCanvas.GetComponent<CanvasGroup>().alpha = 0;
+    toTombCanvas.GetComponent<CanvasGroup>().alpha = 0;
+    enterCanvas.GetComponent<CanvasGroup>().alpha = 0;
+
+    playerController = explorerObject.GetComponent<PlayerController>();
+    explorerController = explorerObject.GetComponent<ExplorerController>();
 
     FadeToWalk();
   }
@@ -39,24 +48,45 @@ public class ExplorerTutorialScript : IntroTutorialScript {
     if ( (Input.GetAxis("Vertical") != 0) && walkCanvas.activeSelf) {
       FadeTo(walkCanvas, pivotCanvas);
     }
-    // if pivot canas activated
+    // if pivot canvas activated
     else if (pivotCanvas.activeSelf) {
       if( EndPivot() )
-        FadeTo(pivotCanvas, runCanvas);
+        FadeTo(pivotCanvas, pickupCanvas);
+    }
+    // if the pickup canvas is activated, fade when player is carrying an object
+    else if (pickupCanvas.activeSelf) {
+      if (playerController.carrying)
+        FadeTo(pickupCanvas, throwCanvas);
+    }
+    // if the throw canvas is activated, fade when player isn't carrying anymore
+    else if (throwCanvas.activeSelf) {
+      if (Input.GetButtonDown("Fire2")) {
+        FadeTo(throwCanvas, toTombCanvas);
+      }
+    }
+    // if the go towards tomb canvas is activated, fade to sprint
+    else if (toTombCanvas.activeSelf) {
+      FadeToSprint();
     }
     // if run canvas activated
     else if( runCanvas.activeSelf ) {
       if( Input.GetKeyDown(KeyCode.LeftShift) ) {
-        FadeTo(runCanvas, extinguishCanvas);
+        Debug.Log("fade run");
+        StartCoroutine(FadeOut(runCanvas, 0.5F));
       }
+    }
+    // if the player took the torch, show message to extinguish
+    else if (explorerController.carryingTorch && !extinguishCanvas.activeSelf && !showedExtinguish) {
+      StartCoroutine(FadeIn(extinguishCanvas, 1F));
+      showedExtinguish = true;
     }
     // if fire pots canvas activated
     else if( extinguishCanvas.activeSelf ){
       // disable default message
       fireInfo.enabled = false;
 
-      if(Input.GetButtonDown("Fire2") ) {
-        StartCoroutine(FadeOut(extinguishCanvas, 1F));
+      if(Input.GetButtonDown("Fire1") ) {
+        FadeTo(extinguishCanvas, enterCanvas);
       }
     }
     // if pickupCanvas activated
@@ -109,7 +139,7 @@ public class ExplorerTutorialScript : IntroTutorialScript {
   }
 
   private void FadeToWalk () {
-    StartCoroutine(WaitFunction(4.0F));
+    StartCoroutine(WaitFunction(4.0F, headCanvas, walkCanvas));
   }
 
   private void FadeTo( GameObject fromCanvas, GameObject toCanvas){
@@ -117,12 +147,15 @@ public class ExplorerTutorialScript : IntroTutorialScript {
     StartCoroutine(FadeIn(toCanvas, 0.5F));
   }
 
+  private void FadeToSprint () {
+    StartCoroutine(WaitFunction(3.0F, toTombCanvas, runCanvas));
+  }
 
-  IEnumerator WaitFunction (float waitTime)
+  IEnumerator WaitFunction (float waitTime, GameObject fromCanvas, GameObject toCanvas)
   {
     yield return new WaitForSeconds(waitTime);
-    StartCoroutine(FadeOut(headCanvas, 0.5F));
-    StartCoroutine(FadeIn(walkCanvas, 0.2F));
+    StartCoroutine(FadeOut(fromCanvas, 0.5F));
+    StartCoroutine(FadeIn(toCanvas, 0.2F));
   }
 
   IEnumerator FadeIn (GameObject obj, float speed) {
