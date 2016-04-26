e06c73c083fbc2ddeb7093b17ff2ba35c858261a
Anda Truta <contact@andatruta.com>
2016-04-26 19:09:13 +0100

    Added timer icon and blinking effect :alarm_clock:

---
+++ Assets/Scripts/TimerBlinkingScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerBlinkingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    StartCoroutine(Blinking());	
+	}
+
+  IEnumerator Blinking () {
+    Debug.Log("blink");
+    float increment;
+    CanvasGroup cv = GetComponent<CanvasGroup>();
+    float speed = 1.0F;
+    while (true) {
+      // fade out
+      while (cv.alpha > 0.4) {
+          increment = speed * Time.deltaTime;
+          if (cv.alpha - increment < 0) cv.alpha = 0;
+          else cv.alpha -= speed * Time.deltaTime;
+          yield return null;
+      }
+      //
+      while (cv.alpha < 1) {
+          increment = speed * Time.deltaTime;
+          if (cv.alpha + increment > 1) cv.alpha = 1;
+          else cv.alpha += speed * Time.deltaTime;
+          yield return null;
+      }
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    // StartCoroutine(Blinking());
+	}
+}
