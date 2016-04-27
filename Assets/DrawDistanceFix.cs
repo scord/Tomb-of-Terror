25e46228e6f45ddb72d5ab5a6c1410baaa423460
Sam Cordingley <samcordingley@gmail.com>
2016-04-27 13:06:15 +0100

    fix draw distance on explorer

---
+++ Assets/DrawDistanceFix.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DrawDistanceFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera cam = GetComponent<Camera>();
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("startscene"))
+            cam.farClipPlane = 400;
+        else cam.farClipPlane = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
