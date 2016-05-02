05c439b1f875e77250ae09dc78d780afe7e181fb
Paul <pp13003@my.bristol.ac.uk>
2016-05-02 19:26:04 +0100

    sound canvases

---
+++ Assets/CatchCanvas.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatchCanvas : MonoBehaviour {
+
+	bool finished = false;
+	private MummyIntroScript canvas;
+
+	public void OnTriggerStay(Collider mummyCollider){
+		if(mummyCollider.tag == "Player"){
+			finished =  mummyCollider.gameObject.GetComponent<MummyController>().CheckTutorial();
+			Debug.Log("works");
+			if(finished){
+				Debug.Log("trigger run ");
+				canvas = mummyCollider.gameObject.transform.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor/OVRTrackerBounds/Canvas").gameObject.GetComponent<MummyIntroScript>();
+				canvas.Catch();
+				Destroy(gameObject);
+			}
+		}
+	}
+}
