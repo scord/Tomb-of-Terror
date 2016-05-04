489f190823fc8c32375fb3a5ca1927db1359f05a
Sam Cordingley <samcordingley@gmail.com>
2016-05-04 19:47:48 +0100

    change update to fixedupdate in ovrplayercontroller

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Awake()
+    {
+        //QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = -1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/OVR/Scripts/Util/OVRPlayerController.cs
+++ Assets/OVR/Scripts/Util/OVRPlayerController.cs
@@ -143,7 +143,7 @@ public class OVRPlayerController : MonoBehaviour
 		}
 	}
 
-	protected virtual void Update()
+	protected virtual void FixedUpdate()
 	{
 		if (useProfileData)
 		{
